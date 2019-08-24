829fb630b360161c2309f59da14adfe11c49252a
Clément Gassmann-Prince <cagap8@gmail.com>
2019-08-23 20:46:16 -0400

    Progress map view created

---
+++ application/views/progress_map.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Carte du cheminement</title>
+</head>
+<body>
+    Carte du cheminement.
+</body>
+</html>
