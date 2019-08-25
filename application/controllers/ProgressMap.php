efe777168dda5bdc13da0d02fc3c724cb001d4c8
Clément Gassmann-Prince <cagap8@gmail.com>
2019-08-25 16:04:19 -0400

    /progress-map/:degree: route now displays only the courses specific to :degree:

--- application/controllers/ProgressMap.php
+++ application/controllers/ProgressMap.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class ProgressMap extends CI_Controller {
     private $course_model = "Course_model";
 
-    public function display()
+    public function display($degree_short_name)
     {
         $this->load->model($this->course_model);
 
-        $this->load->view('progress_map', ['courses' => $this->{$this->course_model}->get_all()]);
+        $this->load->view('progress_map', ['courses' => $this->{$this->course_model}->get_all_from_degree($degree_short_name)]);
     }
 }
