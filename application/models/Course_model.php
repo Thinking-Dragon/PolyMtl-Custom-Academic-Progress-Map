59eb8d689a7e9d3b31e16a33f4cab2091985b531
Clément Gassmann-Prince <cagap8@gmail.com>
2019-08-25 16:33:28 -0400

    Added semester order field when getting all courses of a degree

--- application/models/Course_model.php
+++ application/models/Course_model.php
@@ -18,7 +18,7 @@ class Course_model extends CI_Model {
 
         public function get_all_from_degree($degree_short_name)
         {
-                $this->db->select('sigle, name, credits');
+                $this->db->select('sigle, name, credits, semesterOrder');
                 $this->db->join('DegreesCourses', 'DegreesCourses.idCourse = Courses.idCourse');
                 $this->db->from('Courses');
                 return $this->db->get()->result();
